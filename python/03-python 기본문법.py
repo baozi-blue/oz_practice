37396d02720577a88b6ca0835e3a0e6dc8ec5bb4
baozi-blue <whybaozi@gmail.com>
2024-09-04 18:59:29 +0900

    update

---
+++ python/03-python 기본문법.py
@@ -0,0 +1,21 @@
+print ("hello world")
+
+# 주석 처리 시 # 또는     ctrl or command + /
+# """ """ 사용
+
+"""
+print ("아무말 대잔치)
+
+"""
+
+
+for animals in ("rabbit","lion","fubao","tiger"):
+    print(animals)
+    print("come with me?")
+
+#들여쓰기 자체가 문법이기 때문에 코드 실행 순서가 달라질 수 있음
+for animals in ("rabbit","lion","fubao","tiger"):
+    print(animals)
+print("come with me?")
+
+

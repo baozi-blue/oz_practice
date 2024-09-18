6328587c01ff10699d74f2d7f2ef6b5579f11239
baozi-blue <whybaozi@gmail.com>
2024-09-19 08:37:21 +0900

    update

---
+++ python/과제/7일차 과제/module_package/main_2.py
@@ -0,0 +1,7 @@
+import oz_package.oz_module_1 as one
+import oz_package.oz_module_2 as two
+
+print(one.val_1)
+print(two.val_2)
+
+# 오른쪽 상단 삼각형 실행 버튼으로 실행
---
+++ python/과제/7일차 과제/module_package/main_3.py
@@ -0,0 +1,5 @@
+import animals.birds as one
+import animals.mamals as two
+
+print(one.val_1)
+print(two.val_2)

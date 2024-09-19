5b591894a5f766749c2e595ab7e2a30a9ab9bae7
baozi-blue <whybaozi@gmail.com>
2024-09-19 18:50:32 +0900

    update

---
+++ python/과제/8일차 과제/banking_system/main.py
@@ -0,0 +1,40 @@
+# 메인 함수 구현
+
+#1. BankingService 인스턴스를 생성합니다.
+#2. 사용자로부터 입력을 받아 사용자 추가 및 찾기 기능을 제공합니다.
+#3. 사용자 메뉴를 통해 입금, 출금, 잔고 확인, 거래 내역 기능을 실행할 수 있도록 합니다.
+
+
+
+if __name__ == "__main__":
+    banking_service = BankingService()
+
+    while True:
+        print("1. 사용자 추가")
+        print("2. 사용자 찾기")
+        print("3. 종료")
+
+        choice = input("메뉴를 선택하세요: ")
+
+        if choice == '1':
+            username = input("사용자 이름을 입력하세요: ")
+            account = Account(0, [])  
+            user = User(username, account)
+            banking_service.add_user(user)
+            print(f"{username} 사용자 추가 완료")
+
+        elif choice == '2':
+            username = input("찾을 사용자 이름을 입력하세요: ")
+            user = banking_service.find_user(username)
+            if user:
+                print(f"{username} 사용자 찾기 완료")
+                banking_service.user_menu()
+            else:
+                print("사용자를 찾을 수 없습니다.")
+
+        elif choice == '3':
+            print("프로그램을 종료합니다.")
+            break
+
+        else:
+            print("잘못된 메뉴를 선택했습니다.")

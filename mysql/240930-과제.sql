0e76a1d30ab8871c44b901724dfcc3377151bf87
Emma Wang <whybaozi@gmail.com>
2024-10-02 13:57:24 +0900

    sql practice

---
+++ mysql/240930-과제.sql
@@ -0,0 +1,47 @@
+-- USE classicmodels;
+-- SELECT * FROM customers;
+
+## 1. 생성 (CREATE)
+## customers
+-- INSERT INTO customers (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## products
+-- INSERT INTO products (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## employees
+-- INSERT INTO customers (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## offices
+-- INSERT INTO employees (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## orders
+-- INSERT INTO orders (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## orderdetails
+-- INSERT INTO orderdetails (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## payments
+-- INSERT INTO payments (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## productlines
+-- INSERT INTO productlines (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1000, 'Emma Wang', 'Emma', 'Wang', '303-303-303', '123 Main St', 'Seongnam', 'Gyeonggi-do', 12375, 'Korea');
+
+## customers2
+-- INSERT INTO customers (customerNumber, customerName, contactFirstName, contactLastName, phone, addressLine1, city, state, postalCode, country) 
+-- VALUES (1001, 'Andrew Park', 'Andrew', 'Park', '312-222-455', '456 Lincon St', 'Seodaemun-gu', 'Seoul', 23456, 'Korea');
+
+
+## products2
+
+
+
+
+

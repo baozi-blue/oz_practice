d44aaeaeec302c7f2003d66a6caaaf6702608bf2
Emma Wang <whybaozi@gmail.com>
2024-10-17 10:04:14 +0900

    update

--- oz-flask-backend/smorest/app.py
+++ oz-flask-backend/smorest/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_smorest import Api
-from api import blp
+from api import blp  # api라는 파일에서 blp import시킨다
 
 app = Flask(__name__)
 
-# OpenAPI 관련 설정
+# OpenAPI 관련 설정 (GPT와 관련 없는 tool)
 app.config["API_TITLE"] = "My API"
 app.config["API_VERSION"] = "v1"
 app.config["OPENAPI_VERSION"] = "3.1.3"
@@ -17,3 +17,7 @@ api.register_blueprint(blp)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+# 디버깅 기능 활용해서 스텝 바이 스텝 확인 (왼쪽 빨간점 찍어주기)

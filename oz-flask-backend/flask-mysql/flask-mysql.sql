898f25ad346fec3fff2045154ccdf4ae99661753
Emma Wang <whybaozi@gmail.com>
2024-10-21 14:07:35 +0900

    update

---
+++ oz-flask-backend/flask-mysql/flask-mysql.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE oz;
+-- USE oz; 
+
+-- CREATE TABLE users (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100),
+--     email VARCHAR(100)
+-- );
+
+SELECT * FROM oz.users;
